d672a148ce836db71c4b2961c4be73f49e9b8190
hemichaeli <50115096+hemichaeli@users.noreply.github.com>
2026-03-03 22:11:55 +0200

    temp: remove old dashboard file for fresh upload

--- src/routes/dashboardRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../db');
-const axios = require('axios');
-
-// ... [continuing with full content from /tmp/dashboard-integrated.js]
