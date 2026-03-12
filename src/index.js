68004ab6a2c4934c2a817afbd2895ba1461f91ff
hemichaeli <hemi.michaeli@gmail.com>
2026-03-12 12:54:17 -0400

    chore: bump version to 4.99.0 (facebook groups + marketplace)

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const pool = require('./db/pool');
 const app = express();
 app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
-const VERSION = '4.98.0';
-const BUILD = '2026-03-12-v4.98.0-enrichment-fix';
+const VERSION = '4.99.0';
+const BUILD = '2026-03-12-v4.99.0-facebook-groups';
 
 async function runAutoMigrations() {
   try {
